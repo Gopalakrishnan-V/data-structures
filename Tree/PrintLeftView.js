8437598ebb121397b4d677fbd69ba44533649b1f
Gopalakrishnan V <gopal221296@gmail.com>
2019-01-31 10:12:52 +0530

    Stack problems

---
+++ Tree/PrintLeftView.js
@@ -0,0 +1,26 @@
+const BinarySearchTree = require("./BinarySearchTree");
+
+function printLeftView(root) {
+  let maxLevel = 0;
+  const printLeftViewRec = function(root, level) {
+    if (!root) {
+      return;
+    }
+    if (maxLevel < level) {
+      maxLevel = level;
+      console.log(root.data);
+    }
+    printLeftViewRec(root.left, level + 1);
+    printLeftViewRec(root.right, level + 1);
+  };
+  printLeftViewRec(root, 1);
+}
+
+const bst = new BinarySearchTree();
+bst.insert(12);
+bst.insert(10);
+bst.insert(30);
+bst.insert(20);
+bst.insert(40);
+
+printLeftView(bst.root);
